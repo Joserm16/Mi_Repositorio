eed96de57da712d7ec1c8a44d20f7820a3212552
Jose Ruz Muñoz <jruzmunb@gmail.com>
2024-12-07 12:09:03 +0100

    Historia de usuario Anular Inscripción

--- SolicitudIntercambio.cc
+++ SolicitudIntercambio.cc
@@ -14,14 +14,3 @@ void SolicitudIntercambio::mostrar(){
     
     std::cout << std::endl;
 }
-
-void SolicitudIntercambio::guardarEnFichero(std::string nombreFichero) {
-        std::ofstream fichero(nombreFichero, std::ios::app);  // Abre el fichero en modo 'append'
-        if (fichero.is_open()) {
-            // Escribe los datos en el fichero
-            fichero << id << "," << alumno << "," << universidadDestino << "\n";  
-            std::cout << "Solicitud guardada correctamente en el fichero.\n";
-        } else {
-            std::cerr << "Error al abrir el fichero.\n";
-        }
-}
--- SolicitudIntercambio.h
+++ SolicitudIntercambio.h
@@ -25,7 +25,6 @@ class SolicitudIntercambio {
             activa = false;
         }
 
-        void guardarEnFichero(std::string nombreFichero);
 };
 
 #endif
